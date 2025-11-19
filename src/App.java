c253241f9c301a77f4c8fd5d4296c2391fa6a210
Roberta Parcianello <roberta.parcianello@ifpr.edu.br>
2025-11-19 17:50:21 -0300

    delecao

--- src/App.java
+++ src/App.java
@@ -1,23 +1,70 @@
+//import java.util.List;
+//import java.util.Scanner;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World csv!");
+        int opcao = -1;
         Aluno aluno = new Aluno();
         Scanner scanner = new Scanner(System.in);
-       
-        System.out.println("Informe o Id do aluno");
-        aluno.setId(scanner.nextLine());
-        scanner.nextLine();
-        System.out.println("Informe o nome do aluno");
-        aluno.setNome(scanner.nextLine());
-        scanner.nextLine();
-        System.out.println("Informe o email do aluno");
-        aluno.setEmail(scanner.nextLine());
-        scanner.nextLine();
 
-        GeradorCSV.criarCSV(aluno);
+        do {
+            System.out.println("1 - Incluir");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Atualizar");
+            System.out.println("4 - Deletar");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção (1-4): ");
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Informe o Id do aluno");
+                    scanner.nextLine();
+                    aluno.setId(scanner.nextLine());
+                    scanner.nextLine();
+                    System.out.println("Informe o nome do aluno");
+                    aluno.setNome(scanner.nextLine());
+                    scanner.nextLine();
+                    System.out.println("Informe o email do aluno");
+                    aluno.setEmail(scanner.nextLine());
+                    scanner.nextLine();
+
+                    GeradorCSV.inserirCSV(aluno);
+                    break;
+                case 2:
+                    List<Aluno> alunos = GeradorCSV.listarCSV();
+                    System.out.println("======== Listagem de Alunos Cadastrados ==========");
+                    for (Aluno aluno2 : alunos) {
+                        System.out.println();
+                        System.out.println("Id: " + aluno2.getId());
+                        System.out.println("Nome: " + aluno2.getNome());
+                        System.out.println("email: " + aluno2.getEmail());
+                        System.out.println();
+                        System.out.println("--------------------------------------------------");
+                    }
+
+                case 3:
+
+                    break;
+
+                case 4:
+                    System.out.println("informe o id do aluno a deletar");
+                    scanner.nextLine();
+                    String id = scanner.nextLine();
+                    GeradorCSV.delete(id);
+
+                default:
+                    break;
+            }
+
+        } while (opcao != 0);
+
+        // GeradorCSV.delete("3");
+        
 
     }
 }
--- src/GeradorCSV.java
+++ src/GeradorCSV.java
@@ -1,27 +1,85 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeradorCSV {
 
-    public static void criarCSV(Aluno aluno){
-        Path caminho = Paths.get("alunos.csv");
-        try (
-            BufferedWriter writer = Files.newBufferedWriter(
-                                    caminho,
-                                    StandardCharsets.UTF_8,
-                                    StandardOpenOption.CREATE,
-                                    StandardOpenOption.TRUNCATE_EXISTING)){
-            writer.write(aluno.getId()+";"+aluno.getNome()+";"+aluno.getEmail()); 
-            //writer.newLine();                         
+    public static void inserirCSV(Aluno aluno) {
+        Path caminho = Paths.get("data/alunos.csv");
+
+        try {
+            Files.createDirectories(caminho.getParent());
+/*            BufferedWriter writer = Files.newBufferedWriter(
+                            caminho,
+                            StandardCharsets.UTF_8,
+                            StandardOpenOption.CREATE,
+                            StandardOpenOption.TRUNCATE_EXISTING) */
             
+            try (
+                    FileWriter fw = new FileWriter(caminho.toString(), true);   
+                    BufferedWriter writer = new BufferedWriter(fw)) {  
+                writer.write(aluno.getId() + ";" + aluno.getNome() + ";" + aluno.getEmail());
+                writer.newLine();
+
+            } catch (IOException e) {
+                System.err.println("Deu ruim " + e.getMessage());
+            }
         } catch (IOException e) {
-            System.err.println("Deu ruim "+e.getMessage());
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
     }
+
+    public static List<Aluno> listarCSV(){
+        Path caminho = Paths.get("data/alunos.csv");
+        List<Aluno> alunos = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(caminho)) 
+        {
+            String linha;
+            while ((linha = reader.readLine())!=null) {
+                Aluno aluno = new Aluno();
+                String dados[] = linha.split(";");
+                aluno.setId(dados[0]);
+                aluno.setNome(dados[1]);
+                aluno.setEmail(dados[2]);
+                alunos.add(aluno);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("erro "+e.getMessage());
+        }
+        return alunos;   
+
+    }
+
+    public static void delete(String id){
+          Path caminho = Paths.get("data/alunos.csv");
+        
+          try {
+            List<String> linhas = Files.readAllLines(caminho);
+            List<String> novasLinhas = new ArrayList<>();
+
+            for (String linha : linhas) {
+                String[] dados = linha.split(";");
+                if(!dados[0].equals(id)){
+                    novasLinhas.add(linha);
+                }
+            }
+            Files.write(caminho, novasLinhas);
+            System.out.println("Aluno deletado com sucesso");
+
+
+          } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+          }
+    }
+
 }
